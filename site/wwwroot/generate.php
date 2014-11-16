23ffeab9fcc470fe42cf426200db2c20f9a8084f
Makoto16 <Jonuma1@binghamton.edu>
2014-11-16 13:14:19 -0500

    Updated Commit
    Updated favicon

--- site/wwwroot/generate.php
+++ site/wwwroot/generate.php
@@ -3,11 +3,6 @@ $HEADLINE = $_POST["headline"];
 $SENTENCE = $_POST["sentence"];
 $KEYWORD = $_POST["keyword"];
 
-echo '<link rel="icon" 
-      type="image/png" 
-      href="http://d0od.wpengine.netdna-cdn.com/wp-content/uploads/2012/08/favicon.png">' ;
-
-
 $KEYWORD = str_replace(" ","_",$KEYWORD);
 
 $wiki = file_get_contents('http://en.wikipedia.com/wiki/'.$KEYWORD);
@@ -21,6 +16,7 @@ if(empty($KEYWORD)){
 	
 }else{
 if(strcmp($_POST["type"],'News') == 0){
+	
 	$googlepage = file_get_contents('Google-Pop.html');
 	$googlepage = str_replace("INSERT_HEADLINE", $HEADLINE, $googlepage);
 	$googlepage = str_replace("INSERT_WIKI", substr($p1,0,150)."...", $googlepage);
@@ -29,6 +25,7 @@ if(strcmp($_POST["type"],'News') == 0){
 	echo $googlepage;
 }
 if(strcmp($_POST["type"],'Sports') == 0){
+	
 	$googlepage = file_get_contents('Google-Sports.html');
 	$googlepage = str_replace("INSERT_HEADLINE", $HEADLINE, $googlepage);
 	$googlepage = str_replace("INSERT_WIKI", substr($p1,0,150)."...", $googlepage);
@@ -37,6 +34,7 @@ if(strcmp($_POST["type"],'Sports') == 0){
 	echo $googlepage;
 }
 if(strcmp($_POST["type"],'History') == 0){
+	
 	$googlepage = file_get_contents('HistoryDotCom.html');
 	$googlepage = str_replace("INSERT_HEADLINE", $HEADLINE, $googlepage);
 	$googlepage = str_replace("INSERT_WIKI", substr($p1,0,150)."...", $googlepage);
@@ -45,7 +43,8 @@ if(strcmp($_POST["type"],'History') == 0){
 	echo $googlepage;
 }
 if(strcmp($_POST["type"],'CNN') == 0){
-$KEYWORD = str_replace(" ","_",$KEYWORD);
+	
+	$KEYWORD = str_replace(" ","_",$KEYWORD);
 	$jsrc = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=".$KEYWORD;
 	$json = file_get_contents($jsrc);
 	$jset = json_decode($json, true);
