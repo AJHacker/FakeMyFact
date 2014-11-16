b9cc8f68d813f19d6792ab45eb4fd0252a9806c9
Makoto16 <Jonuma1@binghamton.edu>
2014-11-16 13:42:30 -0500

    Updated Commit
    Update in History.html

--- site/wwwroot/generate.php
+++ site/wwwroot/generate.php
@@ -34,12 +34,18 @@ if(strcmp($_POST["type"],'Sports') == 0){
 	echo $googlepage;
 }
 if(strcmp($_POST["type"],'History') == 0){
-	
+	$KEYWORD = str_replace(" ","_",$KEYWORD);
+	$jsrc = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=".$KEYWORD;
+	$json = file_get_contents($jsrc);
+	$jset = json_decode($json, true);
+	$imageURL =  $jset["responseData"]["results"][0]["url"];
+	$KEYWORD = str_replace("_"," ",$KEYWORD);
 	$googlepage = file_get_contents('HistoryDotCom.html');
 	$googlepage = str_replace("INSERT_HEADLINE", $HEADLINE, $googlepage);
 	$googlepage = str_replace("INSERT_WIKI", substr($p1,0,150)."...", $googlepage);
 	$googlepage = str_replace("INSERT_SENTENCE", $SENTENCE, $googlepage);
 	$googlepage = str_replace("INSERT_KEYWORD", $KEYWORD, $googlepage);
+	$googlepage = str_replace("INSERT_IMAGE", '<img style = "height:300px" src="'.$imageURL.'"/>', $googlepage);
 	echo $googlepage;
 }
 if(strcmp($_POST["type"],'CNN') == 0){
